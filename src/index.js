d8ed86bc31594b71682e812726626cb0b9e8c885
TammyBriggs <tamunotonye.briggs@student.moringaschool.com>
2023-03-24 16:02:17 +0300

    Lab Completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('#create-task-form');
+      const taskList = document.querySelector('#tasks');
+      
+      form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        
+        // Create a new task item
+        const newTask = document.createElement('li');
+        const newTaskDescription = document.querySelector('#new-task-description');
+        newTask.innerText = newTaskDescription.value;
+        
+        // Create a delete button for the task
+        const deleteButton = document.createElement('button');
+        deleteButton.innerText = 'Delete';
+        
+        // Add event listener for delete button
+        deleteButton.addEventListener('click', function() {
+          newTask.remove();
+        });
+        
+        // Append the task and delete button to the task list
+        newTask.appendChild(deleteButton);
+        taskList.appendChild(newTask);
+        
+        // Reset the form so task description bar clears once you hit enter
+        form.reset();
+      });
 });
